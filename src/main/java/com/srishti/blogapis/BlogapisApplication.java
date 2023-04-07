2a2bfd66ea26a50c77dfb21937628228857a5361
SrishtiC99 <iamsri9631@gmail.com>
2023-04-07 10:56:03 +0530

    initial commit

---
+++ src/main/java/com/srishti/blogapis/BlogapisApplication.java
@@ -0,0 +1,13 @@
+package com.srishti.blogapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogapisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srishti/blogapis/BlogapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srishti.blogapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
