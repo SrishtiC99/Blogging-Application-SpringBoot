07dde19175dd98494fc82188be1c9b334050726b
SrishtiC99 <iamsri9631@gmail.com>
2023-04-08 10:52:33 +0530

    added model mapper to map user class to dto class

--- src/main/java/com/srishti/blogapis/BlogapisApplication.java
+++ src/main/java/com/srishti/blogapis/BlogapisApplication.java
@@ -1,7 +1,9 @@
 package com.srishti.blogapis;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogapisApplication {
@@ -9,6 +11,11 @@ public class BlogapisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogapisApplication.class, args);
 		System.out.println("hello");
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
--- src/main/java/com/srishti/blogapis/services/impl/UserServiceImpl.java
+++ src/main/java/com/srishti/blogapis/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.srishti.blogapis.services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -64,24 +68,12 @@ public class UserServiceImpl implements UserService {
     }
 
     private User dtoToEntity(UserDto userDto) {
-        User user = new User();
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setAbout(userDto.getAbout());
-
+        User user = this.modelMapper.map(userDto, User.class);
         return user;
     }
 
     public UserDto entityToDto(User user) {
-        UserDto userDto = new UserDto();
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setPassword(user.getPassword());
-        userDto.setAbout(user.getAbout());
-
+        UserDto userDto = this.modelMapper.map(user, UserDto.class);
         return userDto;
     }
     
