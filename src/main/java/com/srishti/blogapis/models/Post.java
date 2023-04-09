37375db57410c823e0be37176967dc080a0440e0
SrishtiC99 <iamsri9631@gmail.com>
2023-04-09 15:47:16 +0530

    Post entity added

--- src/main/java/com/srishti/blogapis/models/Post.java
+++ src/main/java/com/srishti/blogapis/models/Post.java
@@ -1,36 +1,42 @@
 package com.srishti.blogapis.models;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "post")
 @NoArgsConstructor
 @Getter
 @Setter
-public class Category {
-
+public class Post {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer categoryId;
+    private Integer id;
 
     private String title;
 
-    private String description;
+    private String content;
 
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Post> posts = new ArrayList<>();
-    
+    private String image;
+
+    private Date dateCreated;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    private User user;
+
 }
